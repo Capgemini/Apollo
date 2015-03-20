a051661f2005bc93074cb21d6aaec1404f1de38f
Graham Taylor <graham.x.taylor@capgemini.com>
2015-03-20 17:28:55 +0000

    Fix packer build integration

--- packer/scripts/common/install_consul.sh
+++ packer/scripts/common/install_consul.sh
@@ -8,6 +8,7 @@ sudo chmod +x consul
 sudo mv consul /usr/bin/consul
 sudo mkdir /etc/consul.d
 sudo chmod -R 777 /etc/consul.d
+sudo cp /tmp/consul.conf /etc/init/consul.conf
 
 wget https://dl.bintray.com/mitchellh/consul/${CONSUL_VERSION}_web_ui.zip  -O consul-web_ui.zip
 echo Installing Consul Web UI...
