1c81800c34d3cfba9c012c4d008313d04755ed09
Cam Parry <cam.parry@gmail.com>
2015-03-22 22:03:29 +0000

    Commit for packer files and scripts for terraform

---
+++ packer/scripts/ubuntu/install_haproxy.sh
@@ -0,0 +1,14 @@
+# install HAproxy
+sudo apt-get install -y haproxy
+sudo chmod a+w /etc/rsyslog.conf
+echo '$ModLoad imudp' >> /etc/rsyslog.conf
+echo '$UDPServerAddress 127.0.0.1' >> /etc/rsyslog.conf
+echo '$UDPServerRun 514' >> /etc/rsyslog.conf
+sudo service rsyslog restart
+sup cp /ops/templates/haproxy.cfg /etc/haproxy/haproxy.cfg
+
+# eve upstart
+sudo cp /ops/upstart/haproxy.conf /etc/init/haproxy.conf
+
+# consul config
+echo '{"service": {"name": "haproxy", "tags": ["haproxy"]}}' > /etc/consul.d/haproxy.json
