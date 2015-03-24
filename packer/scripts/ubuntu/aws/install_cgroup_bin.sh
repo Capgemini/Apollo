e6360a1125365785e645b1daa50e945cc055f79a
Graham Taylor <graham.x.taylor@capgemini.com>
2015-03-24 19:15:16 +0000

    #12 - Add a packer template for DO

--- packer/scripts/ubuntu/aws/install_cgroup_bin.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-apt-get install -y cgroup-bin
--- packer/scripts/ubuntu/base.sh
+++ packer/scripts/ubuntu/base.sh
@@ -4,4 +4,4 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get update -y
 
-sudo apt-get -y install git curl libcurl3 default-jre-headless unzip wget python-setuptools python-protobuf
+sudo apt-get -y install git curl libcurl3 default-jre-headless unzip wget python-setuptools python-protobuf cgroup-bin
