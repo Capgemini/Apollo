68695374916423de2e8cf8fc14d9a79cdbcf47a4
Graham Taylor <graham.x.taylor@capgemini.com>
2015-03-30 11:39:31 +0100

    install ruby 1.9.1

--- packer/scripts/ubuntu/base.sh
+++ packer/scripts/ubuntu/base.sh
@@ -3,4 +3,4 @@
 sudo apt-get upgrade -y
 sudo apt-get update -y
 
-sudo apt-get -y install git curl libcurl3 default-jre-headless unzip wget python-setuptools python-protobuf cgroup-bin ruby-dev
+sudo apt-get -y install git curl libcurl3 default-jre-headless unzip wget python-setuptools python-protobuf cgroup-bin ruby1.9.1
