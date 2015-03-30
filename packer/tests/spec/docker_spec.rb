03b52b7aa10d6365fa5100db0934f81e27d4dfb8
Graham Taylor <graham.x.taylor@capgemini.com>
2015-03-30 14:03:05 +0100

    MOAR spec tests!

---
+++ packer/tests/spec/docker_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe package('docker') do
+  it { should be_installed }
+end
+
+describe service('docker') do
+  it { should be_installed }
+  it { should_not be_running }
+end
+
+describe file('/etc/init/docker.override') do
+  it { should be_file }
+end
---
+++ packer/tests/spec/marathon_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe package('marathon') do
+  it { should be_installed }
+end
+
+describe service('marathon') do
+  it { should be_installed }
+  it { should_not be_running }
+end
+
+describe file('/etc/init/marathon.override') do
+  it { should be_file }
+end
+
+descibe file('/etc/marathon/conf/task_launch_timeout') do
+  it { should be_file }
+  its(:content) { should match /300000/ }
+end
