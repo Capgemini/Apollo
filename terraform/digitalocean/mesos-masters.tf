1a50514f82f0aef32b73b853353814ef4fd1adbb
Graham Taylor <graham.x.taylor@capgemini.com>
2015-03-30 09:25:47 +0100

    Correct indentation , private networking = true

--- terraform/digitalocean/mesos-masters.tf
+++ terraform/digitalocean/mesos-masters.tf
@@ -1,10 +1,11 @@
 /* Mesos master instances */
 resource "digitalocean_droplet" "mesos-master" {
-  image    = "${var.image.master}"
-  region   = "${var.region}"
-  count    = "${var.masters}"
-  name     = "capgemini-mesos-master-${count.index}"
-  size     = "${var.instance_size.master}"
+  image              = "${var.image.master}"
+  region             = "${var.region}"
+  count              = "${var.masters}"
+  name               = "capgemini-mesos-master-${count.index}"
+  size               = "${var.instance_size.master}"
+  private_networking = true
   ssh_keys = [
     "${var.ssh_fingerprint}"
   ]
--- terraform/digitalocean/mesos-slaves.tf
+++ terraform/digitalocean/mesos-slaves.tf
@@ -1,12 +1,13 @@
 /* Mesos slave instances */
 resource "digitalocean_droplet" "mesos-slave" {
-  image      = "${var.image.slave}"
-  region     = "${var.region}"
-  count      = "${var.slaves}"
-  name       = "capgemini-mesos-slave-${count.index}"
-  size       = "${var.instance_size.slave}"
-  depends_on = ["digitalocean_droplet.mesos-master"]
-  ssh_keys   = [
+  image              = "${var.image.slave}"
+  region             = "${var.region}"
+  count              = "${var.slaves}"
+  name               = "capgemini-mesos-slave-${count.index}"
+  size               = "${var.instance_size.slave}"
+  depends_on         = ["digitalocean_droplet.mesos-master"]
+  private_networking = true
+  ssh_keys = [
     "${var.ssh_fingerprint}"
   ]
 
