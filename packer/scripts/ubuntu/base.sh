e540df27043fd188d19356a647a7dd517fec8d8d
Cam Parry <cam.parry@gmail.com>
2015-03-30 11:08:47 +0100

    Add kernel upgrades

--- packer/scripts/ubuntu/base.sh
+++ packer/scripts/ubuntu/base.sh
@@ -5,3 +5,13 @@ sudo apt-get upgrade -y
 sudo apt-get update -y
 
 sudo apt-get -y install git curl libcurl3 default-jre-headless unzip wget python-setuptools python-protobuf cgroup-bin
+
+`cd /tmp`
+
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.18-vivid/linux-headers-3.18.0-031800-generic_3.18.0-031800.201412071935_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.18-vivid/linux-headers-3.18.0-031800_3.18.0-031800.201412071935_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.18-vivid/linux-image-3.18.0-031800-generic_3.18.0-031800.201412071935_amd64.deb
+sudo dpkg -i linux-headers-3.18.0-*.deb linux-image-3.18.0-*.deb
+
+# refresh grub bootloader and restart your computer
+sudo update-grub
--- packer/scripts/ubuntu/install_docker.sh
+++ packer/scripts/ubuntu/install_docker.sh
@@ -4,3 +4,5 @@ sudo apt-get -y update
 curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 sudo service docker stop
 echo manual | sudo tee /etc/init/docker.override >/dev/null
+
+-d -s overlay
