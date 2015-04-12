dab46bbeea3d2a349fd9df9de8a6f8fa517452b0
Enxebre <alberto.garcial@hotmail.com>
2015-04-12 21:26:07 +0100

    dynamic specs_path param in run_serverspecs script

--- terraform/aws/serverspecs/run_serverspecs.sh
+++ terraform/aws/serverspecs/run_serverspecs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SPECS_SUITE=$1
-pushd /tmp/serverspecs
+SPECS_PATH={$2:-/tmp/serverspecs}
+pushd ${SPECS_PATH}
 # This can be removed once we rebuild the base image with bundler installed. 
 sudo apt-get -y install bundler
 bundle install --path=vendor && bundle exec rake spec:${SPECS_SUITE}
