0296fa28a3ae8084dc99700061848e0aacfc351c
Enxebre <alberto.garcial@hotmail.com>
2015-04-13 22:53:54 +0100

    increasing spec coverage and fixing weave

--- terraform/aws/serverspecs/run_serverspecs.sh
+++ terraform/aws/serverspecs/run_serverspecs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SPECS_SUITE=$1
-SPECS_PATH={$2:-/tmp/serverspecs}
+SPECS_PATH=${2:-'/tmp/serverspecs'}
 pushd ${SPECS_PATH}
 # This can be removed once we rebuild the base image with bundler installed. 
 sudo apt-get -y install bundler
--- terraform/aws/serverspecs/spec/mesos-master/sample_spec.rb
+++ terraform/aws/serverspecs/spec/mesos-master/sample_spec.rb
@@ -4,7 +4,42 @@ describe package('mesos') do
   it { should be_installed }
 end
 
-describe service('mesos') do
+describe service('mesos-master') do
   it { should be_running }
 end
 
+describe port(5050) do
+  it { should be_listening }
+end
+
+describe package('marathon') do
+  it { should be_installed }
+end
+
+describe service('marathon') do
+  it { should be_running }
+end
+
+describe port(8080) do
+  it { should be_listening }
+end
+
+describe service('consul') do
+  it { should be_running }
+end
+
+describe service('zookeeper') do
+  it { should be_running }
+end
+
+describe port(2181) do
+  it { should be_listening }
+end
+
+describe service('dnsmasq') do
+  it { should be_running }
+end
+
+describe port(53) do
+  it { should be_listening }
+end
