3285e01834e82e8c3b7785912c4e966e22225f63
Graham Taylor <graham.x.taylor@capgemini.com>
2015-04-17 15:23:16 +0100

    Stashing some more changes for digitalocean

--- terraform/digitalocean/outputs.tf
+++ terraform/digitalocean/outputs.tf
@@ -1,6 +1,10 @@
 output "master.1.ip" {
   value = "${digitalocean_droplet.mesos-master.0.ipv4_address}"
 }
+output "slave.1.ip" {
+  value = "${digitalocean_droplet.mesos-slave.0.ipv4_address}"
+}
+
 /*output "master.2.ip" {
   value = "${digitalocean_droplet.mesos-master.1.ipv4_address}"
 }
