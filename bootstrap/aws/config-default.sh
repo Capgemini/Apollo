14882099aaa5a20e1afd8b1f6020664a6b2bb05c
Graham Taylor <gtayzlor@gmail.com>
2015-05-13 18:43:37 +0100

    Fix a bug where the consul DC / mesos cluster name is not getting populated correctly

--- bootstrap/aws/config-default.sh
+++ bootstrap/aws/config-default.sh
@@ -19,5 +19,5 @@ TF_VAR_slave_size=${TF_VAR_slave_size:-m1.medium}
 TF_VAR_slaves=${TF_VAR_slaves:-1}
 TF_VAR_subnet_availability_zone=${TF_VAR_region}
 
-APOLLO_consul_dc=${APOLLO_consul_dc:-$TF_VAR_region}
-APOLLO_mesos_cluster_name=${APOLLO_mesos_cluster_name:-$TF_VAR_region}
+export APOLLO_consul_dc=${APOLLO_consul_dc:-$TF_VAR_region}
+export APOLLO_mesos_cluster_name=${APOLLO_mesos_cluster_name:-$TF_VAR_region}
--- bootstrap/digitalocean/config-default.sh
+++ bootstrap/digitalocean/config-default.sh
@@ -17,5 +17,5 @@ TF_VAR_master_size=${TF_VAR_master_size:-512mb}
 TF_VAR_slave_size=${TF_VAR_slave_size:-512mb}
 TF_VAR_slaves=${TF_VAR_slaves:-1}
 
-APOLLO_consul_dc=${APOLLO_consul_dc:-$TF_VAR_region}
-APOLLO_mesos_cluster_name=${APOLLO_mesos_cluster_name:-$TF_VAR_region}
+export APOLLO_consul_dc=${APOLLO_consul_dc:-$TF_VAR_region}
+export APOLLO_mesos_cluster_name=${APOLLO_mesos_cluster_name:-$TF_VAR_region}
