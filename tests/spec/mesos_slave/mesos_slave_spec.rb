639e208273f7b69730ea0f0eda31cb91a102b0f2
Mike Wallis <mike@dubdubdub.co.uk>
2015-05-14 16:20:49 +0100

    add mesos client tests #182

--- tests/spec/mesos_slave/mesos_slave_spec.rb
+++ tests/spec/mesos_slave/mesos_slave_spec.rb
@@ -4,6 +4,30 @@ describe package('mesos') do
   it { should be_installed }
 end
 
-describe service('mesos-slave') do
-  it { should be_running }
+describe "mesos slave" do
+  describe service('mesos-slave') do
+    it { should be_running }
+  end
+
+  describe file('/etc/mesos/zk') do
+    it            { should be_file }
+    its(:content) { should contain ':2181/mesos' }
+  end
+
+  describe file('/etc/default/mesos-slave') do
+    it            { should be_file }
+    its(:content) { should match /^MASTER=`cat \/etc\/mesos\/zk`$/ }
+  end
+
+  describe file('/etc/mesos-slave') do
+    it { should be_directory }
+  end
+  describe file('/etc/mesos-slave/work_dir') do
+    it            { should be_file }
+    its(:content) { should contain '/tmp/mesos' }
+  end
+
+  describe file('/tmp/mesos') do
+    it { should be_directory }
+  end
 end
