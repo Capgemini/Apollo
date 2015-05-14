f8ffd877a2ae2ee9b5a679f4fbd5c90490c172e5
Graham Taylor <gtayzlor@gmail.com>
2015-05-14 16:25:07 +0100

    Made a start to #176 - consul specs

--- tests/spec/consul/consul_spec.rb
+++ tests/spec/consul/consul_spec.rb
@@ -3,3 +3,26 @@ require 'spec_helper'
 describe service('consul') do
   it { should be_running }
 end
+
+describe file('/etc/init/consul.override') do
+  it { should_not be_file }
+end
+
+describe port(8301) do
+  it { should be_listening }
+end
+
+describe port(8400) do
+  it { should be_listening }
+end
+
+describe port(8600) do
+  it { should be_listening }
+end
+
+describe file('/etc/consul.d/consul.json') do
+  it { should be_file }
+  it { should be_mode 644 }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
