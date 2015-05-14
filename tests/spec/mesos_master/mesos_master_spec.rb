5b2e76150d63be5b50232a477daff6697ff22e80
Mike Wallis <mike@dubdubdub.co.uk>
2015-05-14 14:41:15 +0100

    set quorum to min 2 nodes and cleanup format

--- tests/spec/mesos_master/mesos_master_spec.rb
+++ tests/spec/mesos_master/mesos_master_spec.rb
@@ -9,9 +9,6 @@ describe "mesos master" do
     it { should be_running }
   end
 
-  describe command("curl --silent --show-error --fail --dump-header /dev/stderr --retry 2 http://#{host_inventory['hostname']}:5050/master/health") do
-    its(:stdout)  { should match /HTTP\/1.1 200 OK/ }
-  end
   describe file('/var/lib/mesos') do
     it { should be_directory }
   end
@@ -25,13 +22,12 @@ describe "mesos master" do
 
     describe file('/etc/mesos-master/quorum') do
       it            { should be_file }
-      its(:content) { should contain '1' }
+      its(:content) { should contain '2' }
     end
   end
 
   describe file('/etc/mesos/zk') do
     it            { should be_file }
-    its(:content) { should contain(':2181/mesos') }
   end
 
   describe file('/etc/default/mesos') do
@@ -39,6 +35,7 @@ describe "mesos master" do
     its(:content) { should match /^LOGS=\/var\/log\/mesos/ }
     its(:content) { should match /^ULIMIT="-n 8192"$/ }
   end
+  
   describe file('/etc/default/mesos-master') do
     its(:content) { should match /^PORT=5050/ }
     its(:content) { should match /^ZK=`cat \/etc\/mesos\/zk`/ }
@@ -47,6 +44,11 @@ describe "mesos master" do
   describe port(5050) do
     it { should be_listening }
   end
+
+  describe command("curl --silent --show-error --fail --dump-header /dev/stderr --retry 2 http://#{host_inventory['hostname']}:5050/master/health") do
+    its(:stdout)  { should match /HTTP\/1.1 200 OK/ }
+  end
+
 end
 
 describe "mesos slave" do
