64cc46610edc9ca7c6518aa05da589c6caa115d3
Graham Taylor <gtayzlor@gmail.com>
2015-05-15 16:51:12 +0100

    Initial commit #109 - aws public TF plan

---
+++ terraform/aws-public/variables.tf
@@ -0,0 +1,65 @@
+variable "access_key" {
+  description = "The aws access key."
+  default = ""
+}
+
+variable "secret_key" {
+  description = "The aws secret key."
+  default = ""
+}
+
+variable "key_name" {
+  description = "The aws ssh key name."
+  default = "Apollo"
+}
+
+variable "key_file" {
+  description = "The ssh public key for using with the cloud provider."
+  default = ""
+}
+
+variable "atlas_infrastructure" {
+  description = "The Atlas infrastructure project to join."
+  default = "capgemini/infrastructure"
+}
+
+variable "region" {
+  description = "The AWS region to create resources in."
+  default = "eu-west-1"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for Apollo."
+  default = "eu-west-1b"
+}
+
+variable "slaves" {
+  description = "The number of slaves."
+  default = "1"
+}
+
+variable "masters" {
+  description = "The number of masters."
+  default = "3"
+}
+
+variable "slave_block_device" {
+  description = "Block device for OSD."
+  default = {
+    volume_size = 30
+  }
+}
+
+variable "instance_type" {
+  default = {
+    master = "m1.medium"
+    slave  = "m1.medium"
+  }
+}
+
+variable "atlas_artifact" {
+  default = {
+    master = "capgemini/apollo-mesos-ubuntu-14.04-amd64"
+    slave  = "capgemini/apollo-mesos-ubuntu-14.04-amd64"
+  }
+}
