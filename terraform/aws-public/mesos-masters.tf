56cc43061816e53bb1352059d7e1d9e5b686f539
Graham Taylor <gtayzlor@gmail.com>
2015-05-15 16:51:12 +0100

    Initial commit #109 - aws public TF plan

---
+++ terraform/aws-public/mesos-masters.tf
@@ -0,0 +1,19 @@
+/* Base packer build we use for provisioning master instances */
+resource "atlas_artifact" "mesos-master" {
+  name = "${var.atlas_artifact.master}"
+  type = "aws.ami"
+}
+
+/* Mesos master instances */
+resource "aws_instance" "mesos-master" {
+  instance_type     = "${var.instance_type.master}"
+  ami               = "${replace(atlas_artifact.mesos-master.id, concat(var.region, ":"), "")}"
+  count             = "${var.masters}"
+  key_name          = "${var.key_name}"
+  source_dest_check = false
+  security_groups   = ["${aws_security_group.default.id}"]
+  tags = {
+    Name = "apollo-mesos-master-${count.index}"
+    role = "mesos_masters"
+  }
+}
