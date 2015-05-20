c514448d560dac9bb6efdc01aa812e74c69dbf3e
enxebre <alberto.garcial@hotmail.com>
2015-05-20 10:13:47 +0100

    Terraform now remove the default egress rule so creating them.

--- terraform/aws/security_groups.tf
+++ terraform/aws/security_groups.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "default" {
     self        = true
   }
 
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    self        = true
+  }
+
   tags {
     Name = "apollo-mesos-default-vpc"
   }
@@ -33,6 +40,20 @@ resource "aws_security_group" "nat" {
     from_port = 1194
     to_port   = 1194
     protocol  = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
