dc9847b1844ab6ef3d12cfc251c77ce3700f856b
Graham Taylor <gtayzlor@gmail.com>
2015-05-26 09:49:22 +0100

    Remove aws_public

--- terraform/aws-public/outputs.tf
+++ terraform/aws-public/outputs.tf
@@ -1,6 +1,9 @@
 output "master.1.ip" {
   value = "${aws_instance.mesos-master.0.public_ip}"
 }
+output "master_ips" {
+   value = "${join(",", aws_instance.mesos-master.*.public_ip)}"
+}
 output "slave_ips" {
    value = "${join(",", aws_instance.mesos-slave.*.public_ip)}"
 }
