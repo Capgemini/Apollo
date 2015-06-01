5aa0b98ae190624e1b358b26aeb9dda28e549308
Graham Taylor <gtayzlor@gmail.com>
2015-06-01 16:29:45 +0100

    #270 Download docker images to the packer image

--- packer/scripts/ubuntu/install_docker.sh
+++ packer/scripts/ubuntu/install_docker.sh
@@ -4,5 +4,15 @@ set -o pipefail
 
 sudo apt-get -y update
 curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+
+# Download docker containers to the machine to save download time on
+# provisioning
+docker pull gliderlabs/registrator:master
+docker pull asteris/haproxy-consul:latest
+docker pull weaveworks/weave:${WEAVE_VERSION}
+docker pull mysql:5.5
+docker pull capgemini/apollo-commerce-kickstart:latest
+docker pull mesosphere/marathon:${MARATHON_VERSION}
+
 sudo service docker stop
 echo manual | sudo tee /etc/init/docker.override >/dev/null
--- tests/spec/marathon/marathon_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe package('marathon') do
-  it { should be_installed }
-end
-
-describe service('marathon') do
-  it { should be_running.under('upstart') }
-end
