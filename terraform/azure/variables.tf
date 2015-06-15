a5785523a0f3ac2af734d665d599e06619b1113e
Graham Taylor <gtayzlor@gmail.com>
2015-06-15 17:19:45 +0100

    Initial commit on Azure support

---
+++ terraform/azure/variables.tf
@@ -0,0 +1,36 @@
+variable "azure_settings_file" {
+  description = "The Azure settings file."
+  default = ""
+}
+
+variable "username" {
+  description = "The Azure instance username."
+  default = ""
+}
+
+variable "ssh_key_thumbprint" {
+  description = "The Azure SSH key thumbprint."
+  default = ""
+}
+
+variable "region" {
+  description = "The Azure region to create resources in."
+  default = "North Europe"
+}
+
+variable "slaves" {
+  description = "The number of slaves."
+  default = "1"
+}
+
+variable "masters" {
+  description = "The number of masters."
+  default = "3"
+}
+
+variable "instance_type" {
+  default = {
+    master = "Standard_A1"
+    slave  = "Standard_A1"
+  }
+}
