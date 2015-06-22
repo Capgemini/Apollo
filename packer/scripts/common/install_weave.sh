ea7a3adc246f4ea62f28b3c6dedfa7bc3d46da6d
Cam Parry <cam.parry@gmail.com>
2015-06-22 12:34:00 +0100

    Upgrade weave, marathon and proxy vagrant

--- packer/scripts/common/install_weave.sh
+++ packer/scripts/common/install_weave.sh
@@ -3,6 +3,6 @@ set -eux
 set -o pipefail
 
 sudo wget -O /usr/local/bin/weave \
-    https://github.com/weaveworks/weave/releases/download/${WEAVE_VERSION}/weave
+    https://github.com/weaveworks/weave/releases/download/v${WEAVE_VERSION}/weave
 sudo chmod a+x /usr/local/bin/weave
 
