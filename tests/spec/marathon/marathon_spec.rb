24250d9eaa9ec7018bf9bf478b649cad80bfea58
enxebre <alberto.garcial@hotmail.com>
2015-06-24 15:07:27 +0100

    Run marathon as a docker container.

--- tests/spec/marathon/marathon_spec.rb
+++ tests/spec/marathon/marathon_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
-describe package('marathon') do
-  it { should be_installed }
+describe docker_container('marathon') do
+  it { should be_running }
+  it { should have_volume('/tmp/docker.sock','/var/run/docker.sock') }
+  it { should have_volume('/store','/etc/marathon/store') }
 end
 
 describe port(8080) do
@@ -12,11 +14,15 @@ describe command("/usr/local/bin/marathon-wait-for-listen.sh") do
   its(:exit_status) { should eq 0 }
 end
 
+describe port(8080) do
+  it { should be_listening }
+end
+
 describe service('marathon') do
   it { should be_running.under('upstart') }
 end
 
-describe command("curl -s -XPOST 127.0.0.1:8080/v2/apps -d@spec/marathon/sample.json -H \"Content-Type: application/json\" && sleep 3") do
+describe command("curl -s -XPOST 127.0.0.1:8080/v2/apps -d@spec/marathon/sample.json -H \"Content-Type: application/json\" && sleep 5") do
   its(:exit_status) { should eq 0 }
 end
 
