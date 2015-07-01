f6b39461f15aa093d5a4265f48d4b0a0bc9b08e1
enxebre <alberto.garcial@hotmail.com>
2015-07-01 10:05:35 +0100

    Specs for cadvisor.

---
+++ tests/spec/cadvisor/cadvisor_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe docker_container('cadvisor') do
+  it { should be_running }
+  it { should have_volume('/var/lib/docker','/var/lib/docker') }
+  it { should have_volume('/rootfs','/') }
+  it { should have_volume('/var/run','/var/run') }
+  it { should have_volume('/sys','/sys') }
+end
+
+describe port(8081) do
+  it { should be_listening }
+end
+
+describe service('cadvisor') do
+  it { should be_running }
+end
