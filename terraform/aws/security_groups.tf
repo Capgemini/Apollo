374612c4c87d95f4d6d6869de586afdeea315ef7
Paul M Bauer <paul@bauer.codes>
2015-08-17 13:07:07 -0700

    restrict ELB traffic to 443/SSL
    - configure SSL termination at load balancers

--- terraform/aws/security_groups.tf
+++ terraform/aws/security_groups.tf
@@ -75,6 +75,13 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name = "web-apollo-mesos"
   }
