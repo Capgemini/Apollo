aebfa0943d6c81d9be120bbc138b579c7e11e577
Cam Parry <cam.parry@gmail.com>
2015-09-07 15:30:09 +0100

    Update weave and marathon

--- packer/scripts/ubuntu/base.sh
+++ packer/scripts/ubuntu/base.sh
@@ -10,7 +10,7 @@ sleep 30
 sudo apt-get upgrade -y
 sudo apt-get update -y
 
-sudo apt-get -y install git curl libcurl3 bridge-utils bundler default-jre-headless unzip wget python-setuptools python-protobuf cgroup-bin ruby1.9.1
+sudo apt-get -y install git curl auditd audispd-plugins libcurl3 bridge-utils bundler default-jre-headless unzip wget python-setuptools python-protobuf cgroup-bin ruby1.9.1
 sudo easy_install pip
 
 sudo pip install docker-py==1.1.0
