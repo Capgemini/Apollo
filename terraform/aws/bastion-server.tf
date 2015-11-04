6d189740fefabcb4982f8986770461c5e24a2d11
Rafal B <rafal.baba@capgemini.com>
2015-11-04 14:05:09 +0000

    Upgrading docker version on bastion host - issue #505

--- terraform/aws/bastion-server.tf
+++ terraform/aws/bastion-server.tf
@@ -36,7 +36,13 @@ resource "aws_instance" "bastion" {
       "sudo iptables -t nat -A POSTROUTING -j MASQUERADE",
       "echo 1 | sudo tee /proc/sys/net/ipv4/conf/all/forwarding",
       /* Install docker */
-      "curl -sSL https://get.docker.com/ubuntu/ | sudo sh",
+      /* Add the repository to your APT sources */
+      "sudo -E sh -c 'echo deb https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list'",
+      /* Then import the repository key */
+      "sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D",
+      "sudo apt-get update",
+      /* Install docker-engine */
+      "sudo apt-get install -y docker-engine=${var.docker_version}",
       "sudo service docker start",
       /* Initialize open vpn data container */
       "sudo mkdir -p /etc/openvpn",
