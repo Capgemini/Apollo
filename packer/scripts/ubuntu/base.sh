aa25f55e587ce7f794e6ee6779d71c1b0f5f0aae
Paul Bauer <paul@bauer.codes>
2016-01-21 17:14:29 +0000

    packer: pin pip version
    latest (8.0.0) has a serious bug that prevents docker-py from being
    installed

--- packer/scripts/ubuntu/base.sh
+++ packer/scripts/ubuntu/base.sh
@@ -12,7 +12,7 @@ sudo apt-get upgrade -y
 sudo apt-get update -y
 
 sudo apt-get -y install git curl auditd audispd-plugins libcurl3 bridge-utils bundler unzip wget python-setuptools python-protobuf cgroup-bin ruby2.0
-sudo easy_install pip
+sudo easy_install pip==7.1.2
 
 sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby2.0 1
 sudo update-alternatives --install /usr/bin/gem gem /usr/bin/gem2.0 1
