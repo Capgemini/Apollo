4ec4f3c5693352c5861da0a167135ac7e2372fe9
Graham Taylor <gtayzlor@gmail.com>
2016-02-17 15:34:55 +0000

    Add support for (optionally) weave scope. set eth1 for traefik interface on digitalocean, bump traefik container version

--- bootstrap/digitalocean/config-default.sh
+++ bootstrap/digitalocean/config-default.sh
@@ -9,3 +9,4 @@ export TF_VAR_STATE_ROOT="${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}"
 export APOLLO_consul_dc=${APOLLO_consul_dc:-$TF_VAR_region}
 export APOLLO_mesos_cluster_name=${APOLLO_mesos_cluster_name:-$TF_VAR_region}
 export APOLLO_ansible_ssh_user=core
+export APOLLO_traefik_network_interface=eth1
