f589ea66f3b9baaf07c595e07cd9189de7651dbd
Cam Parry <cam.parry@gmail.com>
2016-02-24 10:39:55 +0000

    ansible 2.0 fixes

--- bootstrap/aws/private-cloud/config-default.sh
+++ bootstrap/aws/private-cloud/config-default.sh
@@ -11,4 +11,4 @@ export ANSIBLE_SSH_ARGS="-F ${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}/ssh.conf
 export TF_VAR_region=${TF_VAR_region:-eu-west-1}
 export APOLLO_consul_dc=${APOLLO_consul_dc:-$TF_VAR_region}
 export APOLLO_mesos_cluster_name=${APOLLO_mesos_cluster_name:-$TF_VAR_region}
-export APOLLO_ansible_ssh_user=core
+export APOLLO_ansible_user=core
