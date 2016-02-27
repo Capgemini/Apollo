a46a78666e3ca0bb77ca7f9d1ed9bc95adbc63ac
Babis Karypidis <vouzounarian@gmail.com>
2016-02-27 21:37:09 +0000

    terraform support for Openstack added, including updating terraform coreos cloud init templates for Rackspace and updating bootstrap util and config-default scripts for rackspace

---
+++ terraform/openstack/modules/mesos_masters/main.tf
@@ -0,0 +1,53 @@
+# Variables
+
+variable "region"                  { default = "LON" }
+variable "instance_type"           { default = "general1-4" }
+variable "image_id"                { default = "40155f16-21d4-4ac1-ad65-c409d94b8c7c" }
+variable "key_pair"                { default = "apollo" }
+variable "public_network_id"       { default = "00000000-0000-0000-0000-000000000000" }
+variable "public_network_name"     { default = "PublicNet" }
+variable "private_network_id"      { default = "11111111-1111-1111-1111-111111111111" }
+variable "private_network_name"    { default = "ServiceNet" }
+variable "etcd_discovery_url_file" {}
+variable "masters"                 { default = "3" }
+variable "slaves"                  { default = "1" }
+variable "etcd_discovery_ready"    { default = "" }
+
+# Resources
+
+resource "template_file" "master_cloud_init" {
+  template               = "${path.module}/master-cloud-config.yml.tpl"
+  vars {
+    etcd_discovery_url   = "${file(var.etcd_discovery_url_file)}"
+    etcd_discovery_ready = "${var.etcd_discovery_ready}"
+    size                 = "${var.masters + var.slaves}"
+  }
+}
+
+resource "openstack_compute_instance_v2" "mesos-master" {
+  region            = "${var.region}"
+  name              = "apollo-mesos-master-${count.index}"
+  flavor_id         = "${var.instance_type}"
+  image_id          = "${var.image_id}"
+  count             = "${var.masters}"
+  key_pair          = "${var.key_pair}"
+  network           =
+    {
+      uuid           = "${var.public_network_id}"
+      name           = "${var.public_network_name}"
+      access_network = "true"
+    }
+  network           =
+    {
+      uuid          = "${var.private_network_id}"
+      name          = "${var.private_network_name}"
+    }
+  config_drive      = "true"
+  user_data         = "${template_file.master_cloud_init.rendered}"
+}
+
+# Outputs
+
+output "master_ips" {
+  value = "${join(",", openstack_compute_instance_v2.mesos-master.*.access_ip_v4)}"
+}
