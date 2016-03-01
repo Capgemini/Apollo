6353493f39e2611517be981ddbeb3c1140b4ef2a
Babis Karypidis <vouzounarian@gmail.com>
2016-03-01 11:57:10 +0000

    removed unecessary default TF var settings in config-default

--- bootstrap/rackspace/config-default.sh
+++ bootstrap/rackspace/config-default.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-export TF_VAR_region=${TF_VAR_region:-LON}
-export TF_VAR_auth_url=${TF_VAR_auth_url:-https://identity.api.rackspacecloud.com/v2.0}
-export TF_VAR_mesos_slaves=${TF_VAR_mesos_slaves:-1}
+export TF_VAR_tenant_name=${TF_VAR_tenant_name:?"Need to set TF_VAR_tenant_name non-empty"}
+export TF_VAR_user_name=${TF_VAR_user_name:?"Need to set TF_VAR_user_name non-empty"}
+export TF_VAR_password=${TF_VAR_password:?"Need to set TF_VAR_password non-empty"}
 
 # Overrides default folder in Terraform.py inventory.
 export TF_VAR_STATE_ROOT="${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}"
+
+export TF_VAR_region=${TF_VAR_region:-LON}
 
 export APOLLO_consul_dc=${APOLLO_consul_dc:-$TF_VAR_region}
 export APOLLO_mesos_cluster_name=${APOLLO_mesos_cluster_name:-$TF_VAR_region}
