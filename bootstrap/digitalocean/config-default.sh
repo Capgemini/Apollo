2bb53315692f7003ceab07b25ff3a39d8f9f8baf
Adam Stankiewicz <sheerun@sher.pl>
2016-03-01 12:47:20 +0100

    Use generated id_rsa file for digitalocean ssh access

--- bootstrap/digitalocean/config-default.sh
+++ bootstrap/digitalocean/config-default.sh
@@ -6,6 +6,8 @@ export TF_VAR_do_token=${TF_VAR_do_token:?"Need to set TF_VAR_do_token non-empty
 # Overrides default folder in Terraform.py inventory.
 export TF_VAR_STATE_ROOT="${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}"
 
+export ANSIBLE_SSH_ARGS="-F ${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}/ssh.config -i ${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}/id_rsa -q"
+
 export APOLLO_consul_dc=${APOLLO_consul_dc:-$TF_VAR_region}
 export APOLLO_mesos_cluster_name=${APOLLO_mesos_cluster_name:-$TF_VAR_region}
 export APOLLO_ansible_ssh_user=core
