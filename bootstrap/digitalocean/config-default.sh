95ddfe41af59e8dab7b1ca02626ce6a7640c51dc
Babis Karypidis <vouzounarian@gmail.com>
2016-03-12 16:39:29 +0000

    various post merge fixes for the rackspace provider after merge with upstream master

--- bootstrap/digitalocean/config-default.sh
+++ bootstrap/digitalocean/config-default.sh
@@ -5,8 +5,6 @@ export TF_VAR_do_token=${TF_VAR_do_token:?"Need to set TF_VAR_do_token non-empty
 
 export ANSIBLE_SSH_ARGS="-F ${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}/ssh.config -i ${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}/id_rsa -q"
 
-export ANSIBLE_SSH_ARGS="-F ${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}/ssh.config -i ${APOLLO_ROOT}/terraform/${APOLLO_PROVIDER}/id_rsa -q"
-
 export APOLLO_consul_dc=${APOLLO_consul_dc:-$TF_VAR_region}
 export APOLLO_mesos_cluster_name=${APOLLO_mesos_cluster_name:-$TF_VAR_region}
 export APOLLO_ansible_ssh_user=core
