a4b08490891e9a548801074e1acb7bb150f6f7fc
Paul Bauer <paul@bauer.codes>
2016-04-05 20:25:17 +0000

    roles: add base
    move network configuration from packer to ansible base module
    fix ephemeral port ranges

--- packer/scripts/ubuntu/base.sh
+++ packer/scripts/ubuntu/base.sh
@@ -22,28 +22,3 @@ echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 echo 'networkaddress.cache.ttl=60' | sudo tee -a /usr/lib/jvm/java-8-oracle/jre/lib/security/java.security
-
-cat << EOF > /etc/security/limits.conf
-* soft nofile 64000
-* hard nofile 64000
-root soft nofile 64000
-root hard nofile 64000
-EOF
-
-cat << EOF > /etc/sysctl.d/60-network.conf
-fs.file-max = 100000
-
-# Increase the ipv4 port range:
-net.ipv4.ip_local_port_range = 1024 65535
-
-# General gigabit tuning:
-net.core.rmem_max = 16777216
-net.core.wmem_max = 16777216
-net.ipv4.tcp_rmem = 4096 87380 16777216
-net.ipv4.tcp_wmem = 4096 65536 16777216
-net.ipv4.tcp_syncookies = 1
-
-# This gives the kernel more memory for tcp which you need with many (100k+) open socket connections
-net.ipv4.tcp_mem = 50576 64768 98152
-net.core.netdev_max_backlog = 2500
-EOF
